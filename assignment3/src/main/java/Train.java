9706e1dfaec208c0cf42b1a0c41c422e236488d8
CEOJINSUNG <jinsung1048@gmail.com>
2022-03-29 18:40:30 +0900

    Create Train.java

---
+++ assignment3/src/main/java/Train.java
@@ -0,0 +1,20 @@
+public class Train extends PublicTransport {
+
+    public Station station;
+
+    @Override
+    public void calculatePayment() {
+        int numberOfStation = station.nStations;
+        double farePerStation = station.farePerStation;
+
+        if (numberOfStation < 5) {
+            // total fare = (base fare)
+            setTotalFare(getBaseFare());
+        } else {
+            // total fare = (base fare) + ((number of station) - 5) * (fare per station)
+            double base = getBaseFare();
+            double update = base + (numberOfStation-5) * farePerStation;
+            setTotalFare(update);
+        }
+    }
+}
