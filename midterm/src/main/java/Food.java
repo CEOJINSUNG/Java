b4cb4689936f0f840cb8ef5b6e3487100603f24a
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-11 12:20:02 +0900

    Update Food.java

--- midterm/src/main/java/Food.java
+++ midterm/src/main/java/Food.java
@@ -1,2 +1,26 @@
-public class Food {
+public abstract class Food implements Payment {
+
+    private int type;
+    private int cost;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public void calculateCost() {
+
+    }
 }
