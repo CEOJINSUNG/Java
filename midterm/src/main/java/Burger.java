4d7dd0fee8e6c117a02fe5a97eec36088eb94d04
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-11 12:19:54 +0900

    Update Burger.java

--- midterm/src/main/java/Burger.java
+++ midterm/src/main/java/Burger.java
@@ -1,2 +1,27 @@
-public class Burger {
+import java.util.ArrayList;
+
+public class Burger extends Food {
+    Cheese cheeseObject;
+    ArrayList<Ingredients> ingredients;
+
+    public Cheese getCheeseObject() {
+        return cheeseObject;
+    }
+
+    public void setCheeseObject(Cheese cheeseObject) {
+        this.cheeseObject = cheeseObject;
+    }
+
+    public ArrayList<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public void calculateCost() {
+        super.calculateCost();
+    }
 }
