a76b6f991e9a2e1111a28c36af170acbd87f024d
CEOJINSUNG <jinsung1048@gmail.com>
2022-04-11 12:19:48 +0900

    Update Beverage.java

--- midterm/src/main/java/Beverage.java
+++ midterm/src/main/java/Beverage.java
@@ -1,2 +1,16 @@
-public class Beverage {
+public class Beverage extends Food {
+    private boolean ice;
+
+    public boolean isIce() {
+        return ice;
+    }
+
+    public void setIce(boolean ice) {
+        this.ice = ice;
+    }
+
+    @Override
+    public void calculateCost() {
+        super.calculateCost();
+    }
 }
